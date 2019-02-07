6a64eaf1634ca051510d497be7b32cfcfa5f9a0a
bajuba <bajuba@users.noreply.github.com>
2019-02-07 09:17:52 -0600

    added constructor and draw method to stage.java

--- src/javabug/Main.java
+++ src/javabug/Main.java
@@ -5,6 +5,10 @@ class Main
 	public static void main(String[] args) 
 	{
 		// TODO Auto-generated method stub
-		System.out.println("Hello");
+		//System.out.println("Hello");
+		
+		Stage myStage = new Stage();
+		System.out.println(myStage.draw());
+		
 	}
 }
--- src/javabug/Stage.java
+++ src/javabug/Stage.java
@@ -1,6 +1,35 @@
 package javabug;
 
-public class Stage {
+public class Stage 
+{
+	String[][] stage = new String[11][11];
+	String blankGround=".";
 	
-	String[][] stage = new String[10][10];
+	Stage()
+	{
+		for(int i = 0; i<=10; i++ )
+		{
+			for(int j =0; j<=10;j++)
+			{
+				this.stage[i][j] = blankGround;
+			}
+		}
+	}
+	
+	public String draw()
+	{
+		String result = "";
+		for(int i = 0; i<=10; i++ )
+		{
+			
+			for(int j =0; j<=10;j++)
+			{
+				result += this.stage[i][j];
+				
+			}
+			result += "\n";
+		}
+		return result;
+	}
+	
 }
