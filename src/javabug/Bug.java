dbe729547afcef693c9830f02614cecfeeb4b416
Ryan Henderson <ryan.ev.henderson@gmail.com>
2019-02-12 08:17:16 -0600

    random move

--- src/javabug/Bug.java
+++ src/javabug/Bug.java
@@ -4,31 +4,38 @@ public class Bug {
 	char body = '§';
 	int Xpos = 0;
 	int Ypos = 0;
-	
-	Bug()
-	{
-		
+
+	Bug() {
+
 	}
-	Bug(char inputBody, int X, int Y)
-	{
+
+	Bug(char inputBody, int X, int Y) {
 		this.body = inputBody;
 		this.Xpos = X;
 		this.Ypos = Y;
 	}
-	public void think()
-	{
+
+	public void think() {
 		moveRandom();
 	}
-	public void moveRandom()
-	{
-		//increase x and y -1, 0, or 1
-		
-		
+
+	public void moveRandom() {
+		int random = (int) (Math.random() * 4);
+		switch (random) {
+		case 0:
+			Xpos++;
+		case 1:
+			Xpos--;
+		case 2:
+			Ypos++;
+		case 3:
+			Ypos--;
+		}
+
 	}
-	public String myPos()
-	{
-		return "I am at Position: " + this.Xpos + "," + this.Ypos; 
+
+	public String myPos() {
+		return "I am at Position: " + this.Xpos + "," + this.Ypos;
 	}
-	
-	
+
 }
