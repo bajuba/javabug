ec4a1143adc602afb3846b971289001db2ef10ae
Ryan Henderson <ryan.ev.henderson@gmail.com>
2019-02-12 08:14:22 -0600

    fix indentation

--- src/javabug/Main.java
+++ src/javabug/Main.java
@@ -1,14 +1,12 @@
 package javabug;
 
-class Main
-{
-	public static void main(String[] args) 
-	{
+public class Main {
+	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		//System.out.println("Hello");
-		
+		// System.out.println("Hello");
+
 		Stage myStage = new Stage();
 		System.out.println(myStage.draw());
-		
+
 	}
 }
--- src/javabug/Stage.java
+++ src/javabug/Stage.java
@@ -1,35 +1,28 @@
 package javabug;
 
-public class Stage 
-{
+public class Stage {
 	String[][] stage = new String[11][11];
-	String blankGround=".";
-	
-	Stage()
-	{
-		for(int i = 0; i<=10; i++ )
-		{
-			for(int j =0; j<=10;j++)
-			{
+	String blankGround = ".";
+
+	public Stage() {
+		for (int i = 0; i <= 10; i++) {
+			for (int j = 0; j <= 10; j++) {
 				this.stage[i][j] = blankGround;
 			}
 		}
 	}
-	
-	public String draw()
-	{
+
+	public String draw() {
 		String result = "";
-		for(int i = 0; i<=10; i++ )
-		{
-			
-			for(int j =0; j<=10;j++)
-			{
+		for (int i = 0; i <= 10; i++) {
+
+			for (int j = 0; j <= 10; j++) {
 				result += this.stage[i][j];
-				
+
 			}
 			result += "\n";
 		}
 		return result;
 	}
-	
+
 }
