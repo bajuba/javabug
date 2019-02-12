0823593eea1f9d16f8fa9f3961da2d089e6af1c9
Keegan Davis <Keegster11@gmail.com>
2019-02-12 09:21:41 -0600

    Updating

--- src/javabug/Main.java
+++ src/javabug/Main.java
@@ -4,49 +4,33 @@ import java.util.Scanner;
 
 class Main
 {
-	public static void main(String[] args) 
-	{
+	
+	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		//System.out.println("Hello");
-		
+		// System.out.println("Hello");
+
 		Stage myStage = new Stage();
-		System.out.println(myStage.draw());
+
+		
 		
 		Bug tim = new Bug();
 		Scanner scanner = new Scanner (System.in);
 		while(true) {
 			tim.moveRandom();
 			System.out.println(tim.myPos());
-			scanner.nextLine()
+			scanner.nextLine();
 		}
-		tim.moveRandom();
-		System.out.println(tim.myPos());
-		tim.moveRandom();
-		System.out.println(tim.myPos());
-		tim.moveRandom();
-		System.out.println(tim.myPos());
-		tim.moveRandom();
-		System.out.println(tim.myPos());
-		tim.moveRandom();
-		System.out.println(tim.myPos());
-		tim.moveRandom();
-		System.out.println(tim.myPos());
-		tim.moveRandom();
-		System.out.println(tim.myPos());
-		tim.moveRandom();
-		System.out.println(tim.myPos());
-		tim.moveRandom();
-		System.out.println(tim.myPos());
-		tim.moveRandom();
-		System.out.println(tim.myPos());
-		tim.moveRandom();
-		System.out.println(tim.myPos());
-		tim.moveRandom();
-		System.out.println(tim.myPos());
-		tim.moveRandom();
-		System.out.println(tim.myPos());
-		tim.moveRandom();
-		System.out.println(tim.myPos());
+		Scanner scanner = new Scanner(System.in);  // Create a Scanner object
 		
+		//world loop render loop
+		while(true)
+		{
+		System.out.println(myStage.draw());
+	    //System.out.println("");
+		tim.moveRandom();
+		//System.out.println(tim.myPos());
+		scanner.nextLine();
+		}
 	}
 }
+
